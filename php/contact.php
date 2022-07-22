76a15b76bd837cbbea2726cc421492c608160bdf
junaid2415 <46064158+junaid2415@users.noreply.github.com>
2022-07-23 00:47:57 +0530

    mail

--- php/contact.php
+++ php/contact.php
@@ -8,7 +8,7 @@ $field_phone = $_POST['phone'];
 
 $field_message = $_POST['message'];
 
-$mail_to = 'to@email.com';
+$mail_to = 'junaidmohammad2415@gmail.com';
 
 $subject = 'Message from a site visitor '.$field_first_name;
 
